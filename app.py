c7a742964eb9f7a0c378b07dd43a8eefdce31781
Puwadol L <alpe_panda@hotmail.co.th>
2022-04-27 19:00:46 +0700

    move ground truth and date picker

--- app.py
+++ app.py
@@ -55,39 +55,13 @@ def generate_wordcloud(df):
 st.set_page_config(layout='wide')
 st.title('Thai Trending Phrase Analysis on Temporal News Dataset')
 
-st.subheader('Select Date')
-st.date_input(label='Select Date',min_value= date(2021,10,1),max_value= date(2021,12,31),value=date(2021,10,1),key='ndate')
-news = '_'.join(str(st.session_state.ndate).split('-'))
-
 c1,c2,c3,c4 = st.columns((2,2,2,2))
 ############################################
 
 with c1:
-    df = pd.read_json('./assets/SMA/'+news+'.json',encoding="utf8")
-    df.rename(columns = {'LCS':'Trend', 'frequency':'Frequency'}, inplace = True)
-    st.subheader('Longest Common Substring')
-    generate_wordcloud(df)
-    generate_grid_options_builder(df)
-    
-############################################
-
-with c2:
-    df = pd.read_json('./assets/Twitter/'+news+'.json',encoding="utf8")
-    df.rename(columns = {'word':'Trend', 'frequency':'Frequency'}, inplace = True)
-    st.subheader('Twitter Reference Method')
-    generate_wordcloud(df)
-    generate_grid_options_builder(df)
-############################################
-
-with c3:
-    df = pd.read_json('./assets/Facebook/'+news+'.json',encoding="utf8")
-    df.rename(columns = {'Key':'Trend', 'Value':'Frequency'}, inplace = True)
-    st.subheader('Facebook Reference Method')
-    generate_wordcloud(df)
-    generate_grid_options_builder(df)
-############################################
-
-with c4:
+    st.subheader('Select Date')
+    st.date_input(label='Select Date',min_value= date(2021,10,1),max_value= date(2021,12,31),value=date(2021,10,1),key='ndate')
+    news = '_'.join(str(st.session_state.ndate).split('-'))
     df = pd.read_csv('./assets/TrendSelect - Ground_Truth.csv')
     st.subheader("Ground Truth")
     df = df[df["date"] == st.session_state.ndate.strftime("%m-%d-%Y")]
@@ -96,3 +70,30 @@ with c4:
             st.write(f'<p style="font-size:25px"> - {df[f"trend{i}"].iloc[0]} </p>', unsafe_allow_html=True)
         else:
             break
+
+############################################
+
+with c2:
+    df = pd.read_json('./assets/SMA/'+news+'.json',encoding="utf8")
+    df.rename(columns = {'LCS':'Trend', 'frequency':'Frequency'}, inplace = True)
+    st.subheader('Longest Common Substring')
+    generate_wordcloud(df)
+    generate_grid_options_builder(df)
+    
+############################################
+
+with c3:
+    df = pd.read_json('./assets/Twitter/'+news+'.json',encoding="utf8")
+    df.rename(columns = {'word':'Trend', 'frequency':'Frequency'}, inplace = True)
+    st.subheader('Twitter Reference Method')
+    generate_wordcloud(df)
+    generate_grid_options_builder(df)
+############################################
+
+with c4:
+    df = pd.read_json('./assets/Facebook/'+news+'.json',encoding="utf8")
+    df.rename(columns = {'Key':'Trend', 'Value':'Frequency'}, inplace = True)
+    st.subheader('Facebook Reference Method')
+    generate_wordcloud(df)
+    generate_grid_options_builder(df)
+############################################
